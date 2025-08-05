281d81b69b40b96112197bdd991c05afe4fdbc13
Amen Hany <amen.hany.2810@gmail.com>
2025-08-05 12:25:19 +0300

    Fixed cors url

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ import User from './models/user.js';
 
 const app = express();
 const corsOptions = {
-    origin: ['http://localhost:5173'],
+    origin: [process.env.FRONTEND_URL],
     credentials: true
 }
 
